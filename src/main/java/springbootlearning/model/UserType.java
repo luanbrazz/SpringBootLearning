a9d9901b5c36eede8abb76b36a77308fd50fc1c2
Luan Braz <luan_silvacpv@hotmail.com>
2024-05-28 16:07:39 -0300

    modeling the user type table

---
+++ src/main/java/springbootlearning/model/UserType.java
@@ -0,0 +1,21 @@
+package springbootlearning.model;
+
+import jakarta.persistence.*;
+
+import java.io.Serializable;
+
+@Entity
+@Table(name = "user_type")
+public class UserType implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "user_type_id")
+    private long id;
+
+    private String name;
+
+    private String description;
+
+}
