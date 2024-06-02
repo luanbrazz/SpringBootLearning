defec88370da5a30bca6782fd515218c07761d56
Luan Braz <luan_silvacpv@hotmail.com>
2024-06-02 14:08:01 -0300

    feat: Initialize Swagger API documentation
    - Integrated Springdoc OpenAPI for API documentation
    - Configured API documentation with title, description, version, contact, and license information
    - Added Springdoc OpenAPI dependency to pom.xml
    - Created SwaggerConfig class to customize API documentation details
    - Verified API documentation accessible at http://localhost:8082/app/swagger-ui/index.html#/

---
+++ src/main/java/springbootlearning/configuration/SwaggerConfig.java
@@ -0,0 +1,29 @@
+package springbootlearning.configuration;
+
+import io.swagger.v3.oas.models.ExternalDocumentation;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI springShopOpenAPI() {
+        return new OpenAPI()
+                .info(new Info().title("Title: Spring Boot Learning API")
+                        .description("Description: Spring Boot Learning API documentation")
+                        .version("v0.0.1")
+                        .contact(new Contact()
+                                .name("Luan Braz")
+                                .url("http://luanbraz.com")
+                                .email("luan_silvacpv@hotmail.com"))
+                        .license(new License().name("License 2.0").url("http://springdoc.org")))
+                .externalDocs(new ExternalDocumentation()
+                        .description("Spring Boot Learning Wiki Documentation")
+                        .url("https://springshop.wiki.github.org/docs"));
+    }
+}
